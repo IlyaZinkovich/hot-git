9dd788a69a95fa1d2fb6f075ef7d846cdf34b1eb
Ilya Zinkovich <ilya.zinkovich@gmail.com>
2018-02-18 19:24:52 +0100

    Removed fxml

--- src/main/java/com/tools/hot/git/presentation/HotGitUI.java
+++ src/main/java/com/tools/hot/git/presentation/HotGitUI.java
@@ -1,22 +1,15 @@
 package com.tools.hot.git.presentation;
 
-import static java.util.Objects.requireNonNull;
-
-import java.net.URL;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class HotGitUI extends Application {
 
   @Override
-  public void start(Stage primaryStage) throws Exception {
-    final ClassLoader classLoader = getClass().getClassLoader();
-    final String fxmlFileName = "index.fxml";
-    final URL fxml = requireNonNull(classLoader.getResource(fxmlFileName)).toURI().toURL();
-    Parent root = FXMLLoader.load(fxml);
+  public void start(Stage primaryStage) {
+    Group root = new Group();
     primaryStage.setTitle("Hot Git");
     primaryStage.setScene(new Scene(root, 300, 275));
     primaryStage.show();
